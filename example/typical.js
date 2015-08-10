604f0f4080cc953ecf85ff6159676e02fe39c7e3
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-08-10 22:49:00 +0100

    fixed options.hide bug.. updated example

--- example/typical.js
+++ example/typical.js
@@ -6,22 +6,11 @@ module.exports = {
             "$ cat input.json | my-app [<options>]",
             "$ my-app <files>"
         ],
-        groups: {
-            main: { 
-                title: "Main options",
-                description: "This group contains the most important options."
-            },
-            _none: "No group"
-        },
-        footer: "Project home: https://github.com/me/my-app",
-        hide: "files"
+        footer: "Project home: [underline]{https://github.com/me/my-app}"
     },
     data: [
-        { name: "help", alias: "h", type: Boolean, description: "Display this usage guide.", group: "main" },
-        { name: "files", alias: "f", type: String, multiple: true, defaultOption: true, description: "The input files to process", group: "main" },
-        { name: "timeout", alias: "t", type: Number, description: "Timeout value in ms", group: "main", typeLabel: "<milliseconds>" },
-        { name: "custom", type: Custom, description: "A custom class instance"}
+        { name: "help", alias: "h", type: Boolean, description: "Display this usage guide." },
+        { name: "src", type: String, multiple: true, defaultOption: true, description: "The input files to process", typeLabel: "<files>" },
+        { name: "timeout", alias: "t", type: Number, description: "Timeout value in ms", typeLabel: "<ms>" }
     ]
 };
-
-function Custom(){}
