7bba30d0cea4ae24a0fa245c9238244f3d38feff
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-09-21 11:07:14 +0100

    tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var test = require("tape");
 var usage = require("../");
-var cliOptions = require("../example/my-app");
+var cliOptions = require("../example/simple");
 
 test("basic", function(t){
-    var result = usage(cliOptions);
-    t.ok(/my-app/.test(result));
+    var result = usage(cliOptions.definitions, cliOptions.options);
+    t.ok(/a typical app/.test(result));
     t.end();
 });
