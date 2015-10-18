735b67b1f590041a1eeff6117e7cce5334580daf
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-18 15:26:27 +0100

    added es6 feature detection and es5 fallback

---
+++ es5/usage-options.js
@@ -0,0 +1,22 @@
+'use strict';
+var arrayify = require('array-back');
+
+module.exports = UsageOptions;
+
+function UsageOptions(options) {
+  options = options || {};
+
+  this.title = options.title;
+
+  this.description = options.description;
+
+  this.synopsis = options.synopsis || options.usage && options.usage.forms || options.forms;
+
+  this.groups = options.groups;
+
+  this.examples = options.examples;
+
+  this.footer = options.footer;
+
+  this.hide = arrayify(options.hide);
+}
