19b96b856250a0f26faca0f1751c72d856f48124
Lloyd Brookes <lloyd.brookes@gmail.com>
2018-11-26 11:56:38 +0000

    replace missing example

---
+++ example/simple.js
@@ -0,0 +1,39 @@
+const getUsage = require('../')
+
+const usage = getUsage([
+  {
+    header: 'A typical app',
+    content: 'Generates something {italic very} important. This is a rather long, but ultimately inconsequential description intended solely to demonstrate description appearance. '
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'help',
+        description: 'Display this usage guide.',
+        alias: 'h',
+        type: Boolean
+      },
+      {
+        name: 'src',
+        description: 'The input files to process. This is some additional text existing solely to demonstrate word-wrapping, nothing more, nothing less. And nothing in between.',
+        type: String,
+        multiple: true,
+        defaultOption: true,
+        typeLabel: '{underline file} ...'
+      },
+      {
+        name: 'timeout',
+        description: 'Timeout value in ms.',
+        alias: 't',
+        type: Number,
+        typeLabel: '{underline ms}'
+      }
+    ]
+  },
+  {
+    content: 'Project home: {underline https://github.com/me/example}'
+  }
+])
+
+console.log(usage)
